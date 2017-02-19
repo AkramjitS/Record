9a09981ae7d24c0cc54150be759efdae1bc08d84
akramjit <akramjit.s@gmail.com>
2017-02-19 04:24:08 -0500

    Cannot have the power button or home button active anything

--- app/src/main/java/com/record/record/BackgroundRecorder.java
+++ app/src/main/java/com/record/record/BackgroundRecorder.java
@@ -1,16 +1,30 @@
 package com.record.record;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SearchEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
 
 
-public class BackgroundRecorder extends IntentService {
+public class BackgroundRecorder extends IntentService implements Window.Callback{
     private static final String TAG = "TESTING";
+    private static int counter = 0;
 
 
     public BackgroundRecorder() {
-        super("BackgroundRecorder");
+        super("BackgroundRecorderDebugName");
     }
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -23,7 +37,134 @@ public class BackgroundRecorder extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.i(TAG, "onHandleIntent");
-        
+        counter++;
+        CharSequence text = "onHandleIntent [" + intent + "], counter: " + counter;
+        Log.i(TAG, text.toString());
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        Log.i(TAG, "dispatchKeyEvent before");
+        if (action == KeyEvent.KEYCODE_HOME || action == KeyEvent.KEYCODE_POWER){
+            Log.i(TAG, "dispatchKeyEvent inside");
+            Intent pushIntent = new Intent(this, BackgroundRecorder.class);
+            this.startService(pushIntent);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean dispatchKeyShortcutEvent(KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean dispatchTrackballEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean dispatchGenericMotionEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public View onCreatePanelView(int featureId) {
+        return null;
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreparePanel(int featureId, View view, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onWindowAttributesChanged(WindowManager.LayoutParams attrs) {
+
+    }
+
+    @Override
+    public void onContentChanged() {
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+
+    }
+
+    @Override
+    public void onPanelClosed(int featureId, Menu menu) {
+
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        return false;
+    }
+
+    @Override
+    public boolean onSearchRequested(SearchEvent searchEvent) {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public ActionMode onWindowStartingActionMode(ActionMode.Callback callback) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public ActionMode onWindowStartingActionMode(ActionMode.Callback callback, int type) {
+        return null;
+    }
+
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+
+    }
+
+    @Override
+    public void onActionModeFinished(ActionMode mode) {
+
     }
 }
--- app/src/main/java/com/record/record/MainActivity.java
+++ app/src/main/java/com/record/record/MainActivity.java
@@ -1,21 +1,38 @@
 package com.record.record;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.database.ContentObserver;
 
 public class MainActivity extends AppCompatActivity {
-
-    int counter = 0;
-
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View v) {
+        Context context = this;
+        Intent pushIntent = new Intent(context, BackgroundRecorder.class);
+        context.startService(pushIntent);
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        Log.i("MAIN", ((Integer)action).toString());
+        if (action == KeyEvent.KEYCODE_POWER || action == KeyEvent.KEYCODE_HOME) {
+            Context context = this;
+            Intent pushIntent = new Intent(context, BackgroundRecorder.class);
+            context.startService(pushIntent);
+        }
+        return super.dispatchKeyEvent(event);
     }
 }
