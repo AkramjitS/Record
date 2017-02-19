49018ecbee0f84d439a8914ae61ac517041e252d
akramjit <akramjit.s@gmail.com>
2017-02-18 22:52:44 -0500

    Added button to main activity

--- app/src/main/java/com/record/record/MainActivity.java
+++ app/src/main/java/com/record/record/MainActivity.java
@@ -2,12 +2,35 @@ package com.record.record;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Currently Pressed     0 Times");
+
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(handleClick);
     }
+
+    private View.OnClickListener handleClick = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            counter++;
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.format("Currently Pressed %5d time%s.", counter, counter == 1 ? "" : "s"));
+        }
+    };
 }
