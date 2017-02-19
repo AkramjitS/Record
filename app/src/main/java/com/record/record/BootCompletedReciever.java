57ceda1060c58f23da0ac7fd8c07e7e0dc4de257
akramjit <akramjit.s@gmail.com>
2017-02-18 21:26:02 -0500

    added reciever for when the device boots

---
+++ app/src/main/java/com/record/record/BootCompletedReciever.java
@@ -0,0 +1,13 @@
+package com.record.record;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class BootCompletedReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
